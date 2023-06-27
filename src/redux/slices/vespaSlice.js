2f3896a9dae1c1570837729f39d88cb799a2ef2e
Yash <yash.solo.000@gmail.com>
2023-06-27 22:35:12 +0600

    Need to fix the issue with delete and add vespa

--- src/redux/slices/vespaSlice.js
+++ src/redux/slices/vespaSlice.js
@@ -63,70 +63,10 @@ export const createNewVespa = createAsyncThunk(
     }
   },
 );
-export const createReservation = createAsyncThunk(
-  'vespas/createReservation',
-  async (reservationData, { rejectWithValue }) => {
-    try {
-      const token = getUserFromLocalStorage();
-      const response = await axios.post(
-        `${BASE_URL}/reservations`,
-        reservationData,
-        {
-          headers: {
-            Authorization: `Bearer ${token.token}`,
-          },
-        },
-      );
-      return response.data;
-    } catch (err) {
-      console.error(err);
-      return rejectWithValue(await err.response.data);
-    }
-  },
-);
-
-export const fetchReservations = createAsyncThunk(
-  'vespas/fetchReservations',
-  async (_, { rejectWithValue }) => {
-    try {
-      const token = getUserFromLocalStorage();
-      const response = await axios.get(`${BASE_URL}/reservations`, {
-        headers: {
-          Authorization: `Bearer ${token.token}`,
-        },
-      });
-      return response.data;
-    } catch (err) {
-      return rejectWithValue(await err.response.data);
-    }
-  },
-);
-
-export const deleteReservation = createAsyncThunk(
-  'vespas/deleteReservation',
-  async (id, { rejectWithValue, getState }) => {
-    try {
-      const token = getUserFromLocalStorage();
-      const response = await axios.delete(`${BASE_URL}/reservations/${id}`, {
-        headers: {
-          Authorization: `Bearer ${token.token}`,
-        },
-      });
-      const state = getState();
-      const filteredReservations = state.vespas.reservations.filter(
-        (reservation) => reservation.id != id,
-      );
-      return filteredReservations;
-    } catch (err) {
-      return rejectWithValue(await err.response.data);
-    }
-  },
-);
 
 const initialState = {
   vespas: [],
-  singleVespa: {},
-  reservations: [],
+  singleVespa: {}
 };
 
 export const vespaSlice = createSlice({
@@ -140,9 +80,6 @@ export const vespaSlice = createSlice({
       const filteredVespas = state.vespas.filter((vespa) => vespa.id != action.payload);
       state.vespas = filteredVespas;
     },
-    addReservation: (state, action) => {
-      state.reservations.push(action.payload);
-    },
   },
   extraReducers: {
     [fetchVespas.fulfilled]: (state, action) => {
@@ -155,18 +92,9 @@ export const vespaSlice = createSlice({
       const deletedVespaId = action.payload;
       state.vespas = state.vespas.filter((vespa) => vespa.id !== deletedVespaId);
     },
-    [createReservation.fulfilled]: (state, action) => {
-      state.reservations.push(action.payload);
-    },
-    [fetchReservations.fulfilled]: (state, action) => {
-      state.reservations = action.payload;
-    },
-    [deleteReservation.fulfilled]: (state, action) => {
-      state.reservations = action.payload;
-    },
   },
 });
 
-export const { removeVespa, addVespa, addReservation } = vespaSlice.actions;
+export const { removeVespa, addVespa, } = vespaSlice.actions;
 
 export default vespaSlice.reducer;
